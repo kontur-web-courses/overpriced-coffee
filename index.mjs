297782f4d1f8111803b2347a036934c40fca201a
Vladislav <melnikow.va@gmail.com>
2020-11-23 21:15:55 +0500

    Мельников Шатилов 8 задач

--- index.mjs
+++ index.mjs
@@ -7,6 +7,8 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+app.use(cookieParser());
+app.use("/static" ,express.static('static'));
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
@@ -21,37 +23,62 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect('/menu');
+  //res.sendFile(path.join(rootDir, "/static/html/index.html"));
 });
+
+let menuItems = [
+  {name: "Americano", image: "/static/img/americano.jpg", price: 999 },
+  {name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
+  {name: "Espresso", image: "/static/img/espresso.jpg", price: 99},
+  {name: "Flat-white", image: "/static/img/flat-white.jpg", price: 149}
+];
+let data = {};
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: menuItems
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  let item;
+  for(let it of menuItems) {
+    if(it.name === req.params.name) {
+      item = it;
+    }      
+  }
+  data[req.cookies.name].push(item);
+  res.redirect('/menu');
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  res.render("cart", {
+    layout: "default",
+    items: data[req.cookies.name]
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  data[req.cookies.name] = [];    
+  res.redirect('/menu');
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  if(req.query.name != undefined) {
+    res.cookie('name', req.query.name);
+    data[req.cookies.name] = data[req.cookies.name] || [];
+    res.redirect('/login');
+  } else {
+    
+    res.render("login", {
+      layout: "default",
+      name: req.cookies.name
+    });
+  }
+  
+  
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
