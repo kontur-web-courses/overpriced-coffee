fc18354af1a7d47d9b63a689c3fbb3bc2773e6c4
Stepanov Mikhail <mihanoid10@yandex.ru>
2020-11-23 20:24:58 +0500

    Task 1-11 done

--- index.mjs
+++ index.mjs
@@ -7,8 +7,23 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+let cart = new Map();
+let history = new Map();
+const coffeeArray = [
+  {
+    name: "Americano",
+    image: "/static/img/americano.jpg",
+    price: 350,
+  },
+  { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 755 },
+  { name: "Espresso", image: "/static/img/espresso.jpg", price: 999 },
+  { name: "Flat-white", image: "/static/img/flat-white.jpg", price: 1350 },
+  { name: "Latte", image: "/static/img/latte.jpg", price: 1500 },]
+
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
+app.use(cookieParser())
+app.use('/static', express.static('static'))
 // Настраиваем пути и дефолтный view
 app.engine(
   "hbs",
@@ -21,37 +36,130 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect('/menu')
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: coffeeArray,
+    title: "Меню"
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  if(getName(req)){
+    if(!cart.has(getName(req)))
+     cart.set(getName(req), new Map())
+  let coffeeName = req.params.name;
+  let tempCart = cart.get(getName(req))
+  if (tempCart.has(coffeeName)) {
+    tempCart.set(coffeeName, tempCart.get(coffeeName) + 1);
+  }
+  else {
+    tempCart.set(coffeeName, 1);
+  }
+  res.redirect('/menu');
+}else
+
+res.redirect('/login')
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  let resArray = []
+  let fullPrice = 0
+  if(getName(req)){
+    if(!cart.has(getName(req)))
+     cart.set(getName(req), new Map());
+    let tempCart = cart.get(getName(req))
+    for(let [name, count] of tempCart){
+      const price = coffeeArray.find(e => e.name === name).price;
+      resArray.push({
+        name: name,
+        count: count,
+        image: `/static/img/${name}.jpg`,
+        pricePerOne: price,
+        price: price * count
+      });
+      fullPrice += price * count;
+    }
+    res.render("cart", {
+      title: "Корзина",
+      layout: "default",
+      items: resArray,
+      fullPrice: fullPrice
+    });
+  }
+else
+  res.redirect('/login')
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  if(getName(req)){
+    if(!history.has(getName(req)))
+      history.set(getName(req), []);
+    let deletingMap = new Map(cart.get(getName(req)))
+    history.get(getName(req)).push(deletingMap);
+    history.set(getName(req), history.get(getName(req)))
+    cart.get(getName(req)).clear();
+    res.redirect('/cart');
+  }
+    else
+      res.redirect('/login')
 });
 
+app.get("/history", (req, res) => {
+  if(getName(req)){
+    if(!history.has(getName(req)))
+      history.set(getName(req), []);
+    let tempHistory = history.get(getName(req));
+    let resArray = [];
+    for(let array of tempHistory){
+      for(let [name, count] of array){
+        const price = coffeeArray.find(e => e.name === name).price;
+        resArray.push({
+          name: name,
+          count: count,
+          image: `/static/img/${name}.jpg`,
+          pricePerOne: price,
+          price: price * count
+        });
+      }
+    }
+    res.render("history", {
+      title: "История заказов",
+      layout: "default",
+      items: resArray
+    });
+  }
+    else
+      res.redirect('/login')
+});
+
+
 app.get("/login", (req, res) => {
-  res.status(501).end();
+    if(req.query.name){
+      if(cart.has(req.query.name))
+        res.status(418).end();
+        else{
+          res.cookie('name', req.query.name);
+          res.redirect('/menu')
+          cart.set(req.query.name, new Map())
+        }
+    }else{
+      let login = 'Аноним'
+      if(getName(req))
+        login = getName(req)
+      res.render("login", {
+        title: "Личный кабинет",
+        layout: "default",
+        login: login
+      });
+    }
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
+
+function getName(req){
+  return req.cookies['name'];
+}
--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -3,6 +3,18 @@ const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
 
 themeSwitch.checked = defaultState;
+if(getCookie('theme') === 'dark'){
+  root.classList.toggle("dark");
+  themeSwitch.checked = true;
+}
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+  document.cookie = themeSwitch.checked ? 'theme=dark' : 'theme=light';
 });
+
+function getCookie(name) {
+  let matches = document.cookie.match(new RegExp(
+    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+  ));
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
