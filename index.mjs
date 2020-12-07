3fa2f09d90a374d518fab3a4f360131cb1910cd8
Smirnov A., Vdovichenko A <>
2020-12-07 20:51:22 +0500

    all tasks done

--- index.mjs
+++ index.mjs
@@ -20,38 +20,142 @@ app.engine(
   })
 );
 
+app.use('/static', express.static('static'));
+app.use(cookieParser());
+
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect('/menu')
 });
 
-app.get("/menu", (_, res) => {
+const drinks = [
+  {
+    name: "Americano",
+    image: "/static/img/americano.jpg",
+    price: 999,
+  },
+  { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
+  { name: "Latte", image: "/static/img/latte.jpg", price: 900 },
+  { name: "Espresso", image: "/static/img/espresso.jpg", price: 600 },
+];
+
+app.get("/menu", (req, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    theme: req.cookies.theme,
+    title: 'Меню Overpriced Coffee',
+    items: drinks,
   });
 });
 
+let users = {};
+
+class User {
+  cart = {
+    order: [],
+    total: 0,
+  };
+
+  history = [];
+
+  constructor(username) {
+    this.username = username;
+  }
+
+  addCartToHistory() {
+    this.history.push(Object.assign({}, this.cart));
+  }
+
+  clearCart() {
+    this.cart.order = [];
+    this.cart.total = 0;
+  }
+}
+
+function addDrink(nameDrink, userName) {
+  const drink = drinks.find(drink => drink.name === nameDrink);
+
+  let user = users[userName];
+  if (!user) {
+    user = new User(userName);
+    users[userName] = user;
+  }
+
+  user.cart.order.push(drink);
+  user.cart.total += drink.price;
+}
+
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  addDrink(req.params.name, req.cookies.login);
+  res.redirect('/menu');
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  const username = req.cookies.login;
+
+  let user = users[username];
+  if (!user) {
+    user = new User(username);
+    users[username] = user;
+  }
+
+  res.render("cart", {
+    layout: "default",
+    theme: req.cookies.theme,
+    title: `Корзина пользователя ${req.cookies.login}`,
+    items : user.cart.order, //userCart.order,
+    total: user.cart.total,//userCart.total,
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  const username = req.cookies.login;
+
+  let user = users[username];
+  if (!user) {
+    res.redirect('/login');
+  }
+
+  user.addCartToHistory();
+  user.clearCart();
+
+  res.redirect('/cart');
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  if (req.query.username) {
+    res.cookie("login", req.query.username);
+    res.redirect('/login');
+  } else {
+    res.render("login", {
+      title: 'Личный кабинет',
+      theme: req.cookies.theme,
+      layout: "default",
+      username: req.cookies.login || "Аноним"
+    });
+  }
+});
+
+app.get("/theme", (req, res) => {
+  let theme = '';
+  if (req.query.value === 'true') {
+    theme = 'dark';
+  }
+  res.cookie('theme', theme);
+  res.status(204).end();
+});
+
+app.get("/history", (req, res) => {
+  let user = users[req.cookies.login];
+  if (!user) {
+    res.redirect('\login');
+  }
+
+  res.render("history", {
+    layout: 'default',
+    title: 'История заказов',
+    theme: req.cookies.theme,
+    items: user.history,
+  })
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -3,6 +3,10 @@ const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
 
 themeSwitch.checked = defaultState;
+
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+  themeSwitch.checked = root.classList.contains("dark");
+
+  fetch(`/theme?value=${root.classList.contains('dark')}`);
 });
