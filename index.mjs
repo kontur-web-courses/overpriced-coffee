ac904180a4a9aa606be5aab197236c08828c40f4
BroEnotiKa <di15425@gmail.com>
2021-05-18 15:40:04 +0500

    tasks done

--- index.mjs
+++ index.mjs
@@ -6,6 +6,42 @@ import cookieParser from "cookie-parser";
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+app.use(cookieParser());
+
+const items = [
+    {
+        name: "Americano",
+        image: "/static/img/americano.jpg",
+        price: 999,
+    },
+    {
+        name: "Cappuccino",
+        image: "/static/img/cappuccino.jpg",
+        price: 999
+    },
+    {
+        name: "Nanachi",
+        image: "/static/img/coffee1.jpg",
+        price: 'Your friend'
+    },
+    {
+        name: "Your girlfriend",
+        image: "/static/img/coffee2.jpg",
+        price: Infinity
+    },
+    {
+        name: 'Flat white',
+        image: "/static/img/flat-white.jpg",
+        price: 101011010
+    },
+    {
+        name: "Totoro",
+        image: "/static/img/coffee3.jpg",
+        price: 'Your life'
+    },
+]
+
+app.use('/static', express.static('static'));
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
@@ -21,29 +57,47 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+    res.redirect('/menu');
 });
 
 app.get("/menu", (_, res) => {
-  res.render("menu", {
-    layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
-  });
+    res.render("menu", {
+        layout: "default",
+        items: items
+    });
 });
+
+let ordersForUsername = {}
 
 app.get("/buy/:name", (req, res) => {
   res.status(501).end();
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+    if (req.cookies.username) {
+        const username = req.cookies.username;
+        let orderItems = [];
+        if (ordersForUsername[username]) {
+            orderItems = ordersForUsername[username].map(order => items.find(item => item.name === order));
+        }
+        let fullPrice = orderItems.reduce((prev, order) => parseInt(order.price) + prev, 0);
+
+        if (isNaN(fullPrice)) {
+            fullPrice = 'U don\'t deserve this';
+        }
+
+        if (req.cookies.username === 'kittygirl') {
+            fullPrice = "It's yours for free :)\n 0"
+        }
+        res.render("cart", {
+            layout: 'default',
+            items: orderItems,
+            fullPrice: fullPrice
+        });
+    }
+    else {
+        res.redirect('/login');
+    }
 });
 
 app.post("/cart", (req, res) => {
