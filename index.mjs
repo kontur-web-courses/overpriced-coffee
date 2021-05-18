ab9c413d8e704a4ef8e81ee23ef6605b86e18809
Darya <dashav1605@mail.ru>
2021-05-18 15:36:22 +0500

    tasks done

--- index.mjs
+++ index.mjs
@@ -7,8 +7,43 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+const coffee = [
+  {
+    name: "Americano",
+    image: "/static/img/americano.jpg",
+    price: 99
+  },
+  {
+    name: "Cappuccino",
+    image: "/static/img/cappuccino.jpg",
+    price: 199
+  },
+  {
+    name: "Latte",
+    image: "/static/img/latte.jpg",
+    price: 229
+  },
+  {
+    name: "Flat white",
+    image: "/static/img/flat-white.jpg",
+    price: 259
+  },
+  {
+    name: "Espresso",
+    image: "/static/img/espresso.jpg",
+    price: 59
+  },
+  {
+    name: "Latte Macchiato",
+    image: "/static/img/latte-macchiato.jpg",
+    price: 299
+  },
+];
+
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
+app.use('/static', express.static('static'));
+app.use(cookieParser());
 // Настраиваем пути и дефолтный view
 app.engine(
   "hbs",
@@ -21,37 +56,62 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+  res.redirect('/menu');
 });
 
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: coffee,
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  let cart;
+  if (req.cookies.cart) {
+    cart = req.cookies.cart;
+  } else {
+    cart = [];
+  }
+  cart.push(req.params.name);
+  res.cookie('cart', cart);
+  res.redirect('/menu');
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  let cart;
+  if(req.cookies.cart)
+    cart = req.cookies.cart;
+  else
+    cart = [];
+
+  const cartWithCoffee = cart.map(name => coffee.find(c => c.name === name));
+  res.render('cart', {
+    layout: "default",
+    items: cartWithCoffee,
+    amount: cartWithCoffee.reduce((res, cur) => cur.price + res, 0),
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  res.cookie('cart', []);
+  res.redirect('/');
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  let username;
+  if (req.query.username) {
+    username = req.query.username;
+  } else if (req.cookies.username) {
+    username = req.cookies.username;
+  } else {
+    username = 'Заядлый кофеман';
+  }
+  res.cookie('username', username);
+  res.render('login', {
+    layout: 'default',
+    username: username
+  });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
