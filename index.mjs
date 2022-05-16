825b6a79e633c722af0f29e67977aa9cf6c1a6c0
innaanikina <avril-avril@mail.ru>
2022-05-16 19:28:01 +0500

    tasks 1-8 done

--- index.mjs
+++ index.mjs
@@ -7,8 +7,16 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+let users = [];
+
+// let cartItems = [];
+
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
+
+app.use('/static', express.static('static'));
+app.use(cookieParser());
+
 // Настраиваем пути и дефолтный view
 app.engine(
   "hbs",
@@ -21,6 +29,7 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
+  res.redirect("/menu");
   res.sendFile(path.join(rootDir, "/static/html/index.html"));
 });
 
@@ -31,27 +40,112 @@ app.get("/menu", (_, res) => {
       {
         name: "Americano",
         image: "/static/img/americano.jpg",
-        price: 999,
+        price: 99,
       },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
+      { name: "Cappuccino",
+        image: "/static/img/cappuccino.jpg",
+        price: 99
+      },
+      { name: "Espresso",
+        image: "/static/img/espresso.jpg",
+        price: 99
+      },
+      { name: "Latte",
+        image: "/static/img/latte.jpg",
+        price: 99
+      },
+      { name: "Latte-macchiato",
+        image: "/static/img/latte-macchiato.jpg",
+        price: 99
+      },
+      { name: "Flat-white",
+        image: "/static/img/flat-white.jpg",
+        price: 99
+      }
     ],
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  let username = "Аноним";
+  try {
+    username = req.cookies.username;
+  } catch (TypeError) {
+    console.dir("no such cookie");
+  }
+  let item = req.params.name;
+  let user = users.filter(e => {
+    return e.name === username
+  })[0];
+
+  let userCart = user.cartItems;
+
+  userCart.push({
+    name: item,
+    image: "/static/img/" + item.toLowerCase() + ".jpg",
+    price: 99
+  });
+  res.redirect("/menu");
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  let username = "Аноним";
+  try {
+    username = req.cookies.username;
+  } catch (TypeError) {
+    console.dir("no such cookie");
+  }
+  let user = users.filter(e => {
+    return e.name === username
+  })[0];
+
+  let userCart = user.cartItems;
+
+  let total = 0;
+  userCart.forEach(function(e) {
+    total += e.price;
+  });
+
+  res.render("cart", {
+    layout: "default",
+    items: userCart,
+    total: total
+  });
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  let username = "Аноним";
+  try {
+    username = req.cookies.username;
+  } catch (TypeError) {
+    console.dir("no such cookie");
+  }
+  let user = users.filter(e => {
+    return e.name === username
+  })[0].cartItems = [];
+  res.redirect("/cart");
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  let username = "Аноним";
+  try {
+    username = req.cookies.username;
+  } catch (TypeError) {
+    console.dir("no such cookie");
+  }
+
+  res.render("login", {
+    layout: "default",
+    username: username
+  });
+
+  let name = req.query.username;
+  if (name) {
+    res.cookie('username', name);
+    users.push({name: name, cartItems: []});
+  }
+
 });
+
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
