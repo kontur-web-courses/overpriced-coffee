cd6e213f233e412b11eb3df0a3bb8afcafa1c65d
lessy2014 <lessik03@gmail.com>
2022-05-16 12:11:25 +0500

    1-8 + 10

--- index.mjs
+++ index.mjs
@@ -7,51 +7,104 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+app.use('/static', express.static('static'));
+app.use(cookieParser());
+
+const coffeeItems = [
+  {
+    name: "Americano",
+    image: "/static/img/americano.jpg",
+    price: 999,
+  },
+  {
+    name: "Cappuccino",
+    image: "/static/img/cappuccino.jpg",
+    price: 999},
+  {
+    name: "flat-white",
+    image: "/static/img/flat-white.jpg",
+    price: 300},
+  {
+    name: "Latte-macchiato",
+    image: "/static/img/latte-macchiato.jpg",
+    price: 1337.99
+  }]
+
+const carts = {}
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
 app.engine(
-  "hbs",
-  hbs({
-    extname: "hbs",
-    defaultView: "default",
-    layoutsDir: path.join(rootDir, "/views/layouts/"),
-    partialsDir: path.join(rootDir, "/views/partials/"),
-  })
+    "hbs",
+    hbs({
+      extname: "hbs",
+      defaultView: "default",
+      layoutsDir: path.join(rootDir, "/views/layouts/"),
+      partialsDir: path.join(rootDir, "/views/partials/"),
+    })
 );
 
-app.get("/", (_, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/index.html"));
+app.get("/", (req, res) => {
+  res.redirect("/menu")
 });
 
-app.get("/menu", (_, res) => {
+app.get("/menu", (req, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
-    ],
+    items: coffeeItems,
+    darkThemeOn: (req.cookies.dark_theme === "true" ?? false) ? "checked" : "",
+    title: 'МЕНЮ',
+  });
+});
+
+app.get("/cart", (req, res) => {
+  if (!carts[req.cookies.name])
+    carts[req.cookies.name] = []
+  res.render('cart', {
+    layout: 'default',
+    fullPrice: carts[req.cookies.name].reduce((result, currentValue) => currentValue.price + result, 0),
+    items: carts[req.cookies.name],
+    darkThemeOn: (req.cookies.dark_theme === "true" ?? false) ? "checked" : "",
+    title: "КОРЗИНА"
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  if (!carts[req.cookies.name]){
+    res.redirect("/login")
+  }
+  carts[req.cookies.name].push({...(coffeeItems.find(c => c.name === req.params.name))});
+  res.redirect("/menu");
 });
 
-app.get("/cart", (req, res) => {
-  res.status(501).end();
-});
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  carts[req.cookies.name] = [];
+  res.redirect("/menu");
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  let name;
+  if (req.query.username) {
+    name = req.query.username;
+    res.cookie("name", name);
+  } else if (req.cookies.name) {
+    name = req.cookies.name;
+  }
+  else
+    name = "RandomMammoth";
+
+  if (!carts[name]) {
+    carts[name] = [];
+  }
+
+  res.render("login",
+      {
+        layout: "default",
+        username: name,
+        darkThemeOn: (req.cookies.dark_theme === "true" ?? false) ? "checked" : "",
+        title: "ВХОД"
+      });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
--- static/js/theme.mjs
+++ static/js/theme.mjs
@@ -2,7 +2,8 @@ const themeSwitch = document.querySelector(".theme__switch input");
 const root = document.querySelector(":root");
 const defaultState = root.classList.contains("dark");
 
-themeSwitch.checked = defaultState;
+themeSwitch.checked && root.classList.toggle("dark");
 themeSwitch.addEventListener("click", () => {
   root.classList.toggle("dark");
+  document.cookie = `dark_theme=${themeSwitch.checked}`;
 });
