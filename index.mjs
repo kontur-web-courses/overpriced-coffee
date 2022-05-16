6e96de8e9e8f57cdffc7348a1b36a79c965d6d34
Ksyusha <astakhova.ksyusha.07@mail.ru>
2022-05-16 18:51:03 +0500

    done 8

--- index.mjs
+++ index.mjs
@@ -7,6 +7,9 @@ const rootDir = process.cwd();
 const port = 3000;
 const app = express();
 
+app.use('/static', express.static('static'));
+app.use(cookieParser());
+
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
 // Настраиваем пути и дефолтный view
@@ -21,10 +24,17 @@ app.engine(
 );
 
 app.get("/", (_, res) => {
+  res.redirect('/menu');
   res.sendFile(path.join(rootDir, "/static/html/index.html"));
 });
 
-app.get("/menu", (_, res) => {
+app.get("/menu", (req, res) => {
+  if (!('cart' in req.cookies)){
+    res.cookie('cart', []);
+  }
+  else{
+    res.cookie('cart', req.cookies.cart);
+  }
   res.render("menu", {
     layout: "default",
     items: [
@@ -34,24 +44,63 @@ app.get("/menu", (_, res) => {
         price: 999,
       },
       { name: "Cappuccino", image: "/static/img/cappuccino.jpg", price: 999 },
+      {
+        name: "Latte",
+        image: "/static/img/latte.jpg",
+        price: 10000,
+      },
+      {
+        name: "Flat-white",
+        image: "/static/img/flat-white.jpg",
+        price: 10000,
+      },
     ],
   });
 });
 
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+  const coffeeName = req.params.name;
+  const cart = req.cookies.cart;
+  cart.push(new Order(coffeeName, `/static/img/${coffeeName.toLowerCase()}.jpg`, 10000));
+  res
+    .cookie('cart', cart)
+    .redirect("/menu");
 });
 
 app.get("/cart", (req, res) => {
-  res.status(501).end();
+  const prices = [];
+  const cart = req.cookies.cart;
+  for (const order of cart){
+    prices.push(order.price);
+  }
+  res.render("cart", {
+    layout: "default",
+    total_price: prices.reduce((a, b) => a + b, 0),
+    items: cart,
+  });
+  res.cookie('cart', cart);
 });
 
 app.post("/cart", (req, res) => {
-  res.status(501).end();
+  res.cookie('cart', []);
+  res.redirect('/menu');
 });
 
 app.get("/login", (req, res) => {
-  res.status(501).end();
+  res
+    .cookie('username', req.query.username)
+    .render('login', {
+    layout: "default", username: req.cookies.username,
+  });
 });
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
+
+
+class Order {
+  constructor(name, image, price) {
+    this.name = name;
+    this.image = image;
+    this.price = price;
+  }
+}
