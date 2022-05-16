87b673a380d98cbac577a9291e065354abfce703
Hvv <hvv7007@gmail.com>
2022-05-16 11:19:48 +0500

    4-5 tasks

--- index.mjs
+++ index.mjs
@@ -6,6 +6,43 @@ import cookieParser from "cookie-parser";
 const rootDir = process.cwd();
 const port = 3000;
 const app = express();
+const coffee = [
+  {
+    name: "Americano",
+    image: "/static/img/americano.jpg",
+    price: 999,
+  },
+  {
+    name: "Cappuccino",
+    image: "/static/img/cappuccino.jpg",
+    price: 999,
+  },
+  {
+    name: "Latte",
+    image: "/static/img/latte.jpg",
+    price: 9992,
+  },
+  {
+    name: "Espresso",
+    image: "/static/img/espresso.jpg",
+    price: 9991,
+  },
+  {
+    name: "Flat-White",
+    image: "/static/img/flat-white.jpg",
+    price: 9990,
+  },
+  {
+    name: "Latte-Macchiato",
+    image: "/static/img/latte-macchiato.jpg",
+    price: 9999,
+  },
+  {
+    name: "Flag",
+    image: "/static/img/flag.jpg",
+    price: 1e9,
+  },
+];
 
 // Выбираем в качестве движка шаблонов Handlebars
 app.set("view engine", "hbs");
@@ -20,6 +57,10 @@ app.engine(
   })
 );
 
+app.get('/', (req, res) => {
+  res.redirect('/menu')
+})
+
 app.get("/", (_, res) => {
   res.sendFile(path.join(rootDir, "/static/html/index.html"));
 });
@@ -27,52 +68,26 @@ app.get("/", (_, res) => {
 app.get("/menu", (_, res) => {
   res.render("menu", {
     layout: "default",
-    items: [
-      {
-        name: "Americano",
-        image: "/static/img/americano.jpg",
-        price: 999,
-      },
-      {
-        name: "Cappuccino",
-        image: "/static/img/cappuccino.jpg",
-        price: 999,
-      },
-      {
-        name: "Latte",
-        image: "/static/img/latte.jpg",
-        price: 9992,
-      },
-      {
-        name: "Espresso",
-        image: "/static/img/espresso.jpg",
-        price: 9991,
-      },
-      {
-        name: "Flat-White",
-        image: "/static/img/flat-white.jpg",
-        price: 9990,
-      },
-      {
-        name: "Latte-Macchiato",
-        image: "/static/img/latte-macchiato.jpg",
-        price: 9999,
-      },
-      {
-        name: "Flag",
-        image: "/static/img/flag.jpg",
-        price: 1e9,
-      },
-    ],
+    items: coffee,
   });
 });
 
+let ordered_coffee = [];
+
 app.get("/buy/:name", (req, res) => {
-  res.status(501).end();
+
+  let new_coffee = { ...(coffee.find(item => item.name === req.params.name)) };
+  ordered_coffee.push(new_coffee);
+
+  res.redirect("/menu");
 });
 
 app.get("/cart", (req, res) => {
-  res.sendFile(path.join(rootDir, "/static/html/cart.html"));
+  res.render("cart", {
+    layout: "default",
+    items: ordered_coffee,
+    sum: ordered_coffee.reduce((sum, coffee) => sum + coffee.price, 0),
+  });
 });
 
 app.post("/cart", (req, res) => {
@@ -84,9 +99,5 @@ app.get("/login", (req, res) => {
 });
 
 app.use('/static', express.static('static'))
-
-app.get('/', (req, res) => {
-  res.redirect('/menu')
-})
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
